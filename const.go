2a365117a844c7d9f3517db4a3b20b371af061ee
Umair <umairnehri9747@Gmail.com>
2024-09-27 01:39:51 +0530

    Add files via upload

--- const.go
+++ const.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"time"
 )
@@ -31,14 +32,74 @@ var (
 )
 
 func init() {
+	tmpDir := "/tmp/"
+	matchersFile := tmpDir + "matchers.json"
+
+	// Check if /tmp/ directory exists
+	if _, err := os.Stat(tmpDir); os.IsNotExist(err) {
+		fmt.Println("Error: /tmp/ directory does not exist. Falling back to default matchers.json.")
+		readDefaultMatchers()
+		return
+	}
+
+	// Check if matchers.json exists in /tmp/
+	if _, err := os.Stat(matchersFile); os.IsNotExist(err) {
+		downloadMatchers(matchersFile)
+	}
+
+	// Read matchers.json from /tmp/
+	file, err := ioutil.ReadFile(matchersFile)
+	if err != nil {
+		fmt.Println("Error reading matchers.json from /tmp/:", err)
+		readDefaultMatchers()
+		return
+	}
+
+	if err := json.Unmarshal(file, &matchers); err != nil {
+		fmt.Println("Error unmarshaling matchers:", err)
+		readDefaultMatchers()
+	}
+}
+
+func readDefaultMatchers() {
 	file, err := ioutil.ReadFile("matchers.json")
 	if err != nil {
-		fmt.Println("Error reading matchers.json:", err)
+		fmt.Println("Error reading default matchers.json:", err)
 		os.Exit(1)
 	}
 	if err := json.Unmarshal(file, &matchers); err != nil {
-		fmt.Println("Error unmarshaling matchers:", err)
+		fmt.Println("Error unmarshaling default matchers:", err)
 		os.Exit(1)
+	}
+}
+
+func downloadMatchers(dest string) {
+	url := "https://raw.githubusercontent.com/umair9747/4oFour/refs/heads/main/matchers.json"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error downloading matchers.json:", err)
+		readDefaultMatchers()
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: received non-200 response code:", resp.StatusCode)
+		readDefaultMatchers()
+		return
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		readDefaultMatchers()
+		return
+	}
+
+	if err := ioutil.WriteFile(dest, data, 0644); err != nil {
+		fmt.Println("Error writing matchers.json to /tmp/:", err)
+		readDefaultMatchers()
+		return
 	}
 }
 
